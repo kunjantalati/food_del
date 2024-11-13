d6e843447e608f217ed68c8ae0269a8c5afe8b03
kunjantalati <talatikunj23@gmail.com>
2024-11-13 17:07:08 +0530

    doubled the line

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -8,6 +8,8 @@ const FoodDisplay = ({ category }) => {
     return (
         <div className='food-display' id='food-display'>
             <h2>Top dishes near you</h2>
+            <h2>Top dishes near you</h2>
+
             <div className="food-display-list">
                 {food_list.map((item,index)=>{
                     return <Fooditem key={index}  id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
